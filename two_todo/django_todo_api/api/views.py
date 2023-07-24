ea73b72e323a577a972a3a953bbf39f107573575
shawal-mbalire <mbalireshawal@gmail.com>
2023-07-24 19:38:20 +0300

    django api

--- two_todo/django_todo_api/api/views.py
+++ two_todo/django_todo_api/api/views.py
@@ -1,11 +1,17 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from base.models import Todo
+from .serialisers import TodoSerializer
 
 @api_view(['GET'])
 def getData(request):
-    data = {
-        'name': 'John Doe',
-        'age': 25,
-        'occupation': 'Software Engineer'
-    }
-    return Response(data)
+    todos = Todo.objects.all()
+    serializer = TodoSerializer(todos, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def addTodo(request):
+    serialiser = TodoSerializer(data=request.data)
+    if serialiser.is_valid():
+        serialiser.save()
+    return Response(serialiser.data)
