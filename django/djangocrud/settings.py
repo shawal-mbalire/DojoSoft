4b2bd8c95c335f1cefe6daf52b7fedaedce58984
Shawal Mbalire <mbalireshawal@gmail.com>
2024-11-17 22:04:52 +0300

    Update settings.py

--- django/djangocrud/settings.py
+++ django/djangocrud/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-acv5sghj(2go20pqt6n&lrf&*u7hw^tpclx8*dk-$^x#4n4om$'
+# SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
